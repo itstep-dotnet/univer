da3562eeefffcd6bb18d465a5096f86e472f8d7f
Саня FPV <andrei.cazanov2017@gmail.com>
2020-12-10 18:44:23 +0300

    Добавил класс SashaFPV.cs в ClassLibrary1

---
+++ ClassLibrary1/SashaFPV.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class SashaFPV
+    {
+        public int Id { get; set; }
+        public string Data { get; set; }
+        public string MineName { get; set; } = "Это я, Белошицкий Александр";
+    }
+}
